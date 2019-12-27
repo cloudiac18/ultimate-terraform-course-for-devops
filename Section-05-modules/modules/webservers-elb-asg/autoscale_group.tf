4469b7d3ed8a429d923b84f15651f46472960744
krishnendu sasmal <sasmal1978@hotmail.com>
2019-12-26 20:01:23 -0800

    updated systax

--- Section-05-modules/modules/webservers-elb-asg/autoscale_group.tf
+++ Section-05-modules/modules/webservers-elb-asg/autoscale_group.tf
@@ -1,6 +1,6 @@
 resource "aws_autoscaling_group" "my_first_asg" {
 	name = "${var.cluster}-websever-asg"
- launch_configuration = "${aws_launch_configuration.my-first-launch-conf.id}"
+ launch_configuration = "aws_launch_configuration.my-first-launch-conf.id"
  load_balancers = ["${aws_elb.my_first_elb.name}"]
  availability_zones = "${var.azs}"  
 	min_size = 2
