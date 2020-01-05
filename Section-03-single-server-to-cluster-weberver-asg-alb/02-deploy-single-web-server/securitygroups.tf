f5309a3b293be053c8ce89d1e1a2cf9c6c2184b4
krishnendu sasmal <sasmal1978@hotmail.com>
2020-01-05 11:01:06 -0800

    updated user data syntax

--- Section-03-single-server-to-cluster-weberver-asg-alb/02-deploy-single-web-server/securitygroups.tf
+++ Section-03-single-server-to-cluster-weberver-asg-alb/02-deploy-single-web-server/securitygroups.tf
@@ -4,14 +4,14 @@ resource "aws_security_group" "webserver_sg" {
         from_port = 80
         to_port = 80
         protocol = "tcp"
-        cidr_blocks = [ "73.241.51.131/32"]
+        cidr_blocks = [ "98.207.180.245/32"]
     }
 
     ingress {
         from_port = 22
         to_port = 22
         protocol = "tcp"
-        cidr_blocks = [ "73.241.51.131/32"]
+        cidr_blocks = [ "98.207.180.245/32"]
     }
 egress {
     from_port       = 0
--- Section-03-single-server-to-cluster-weberver-asg-alb/02-deploy-single-web-server/webserver.tf
+++ Section-03-single-server-to-cluster-weberver-asg-alb/02-deploy-single-web-server/webserver.tf
@@ -7,14 +7,15 @@ resource "aws_instance" "My-Webserver" {
 	 Name = "My-Webserver"
  }
  key_name = "terraform"
- #!/bin/bash -xe
-    exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
-    /usr/bin/apt-get update
-    DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get upgrade -yq
-    /usr/bin/apt-get install apache2 -y
-    /usr/sbin/ufw allow in "Apache Full"
-    /bin/echo "Hello world " >/var/www/html/index.html
-    instance_ip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
-    echo $instance_ip >>/var/www/html/index.html
+ user_data = <<EOF
+#!/bin/bash -xe
+exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+/usr/bin/apt-get update
+DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get upgrade -yq
+/usr/bin/apt-get install apache2 -y
+/usr/sbin/ufw allow in "Apache Full"
+/bin/echo "Hello world " >/var/www/html/index.html
+instance_ip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+echo $instance_ip >>/var/www/html/index.html
 EOF
 }
